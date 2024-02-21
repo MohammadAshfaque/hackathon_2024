cbbd7bde47edc2c0e3d6c630792bb6cdb2ca3f80
S A U R A V <sauravchp2@gmail.com>
2024-02-21 23:17:10 +0530

    refactor: Update Footer component

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Logo from "./Logo";
 
 const Footer = () => {
-  return <div>Footer</div>;
+    return (
+        <div>
+            <Logo />
+        </div>
+    );
 };
 
 export default Footer;
