2743649b8f34b55565f9947b3fdb19e7ae9ded97
S A U R A V <sauravchp2@gmail.com>
2024-02-21 23:19:12 +0530

    chore: Reorder components in index.js

--- Components/index.js
+++ Components/index.js
@@ -1,12 +1,12 @@
-import Footer from "./Footer";
-import NavBar from "./NavBar";
-import Hero from "./Hero";
-import PupUp from "./PupUp";
-import Card from "./Card";
 import Logo from "./Logo";
+import Hero from "./Hero";
+import Card from "./Card";
 import Menu from "./menu";
 import ICON from "./ICON";
+import PupUp from "./PupUp";
 import Close from "./Close";
 import Arrow from "./Arrow";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 export { NavBar, Footer, Hero, Card, PupUp, Logo, Menu, ICON, Close, Arrow };
