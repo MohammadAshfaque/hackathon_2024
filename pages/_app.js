991d4588d635a53dfa84a496387326b691dc9769
Md Arman <fahad12387@outlook.com>
2024-02-22 22:34:04 +0530

    update: update app js  Please enter the commit message for your changes. Lines starting

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 
 // Internal import
 import { NavBar, Footer } from "../Components";
+import {CrowdFundingProvide} from "../Context/CrowdFunding";
 
 export default function App({ Component, pageProps }) {
     return (
         <>
+        <CrowdFundingProvide/>
             <NavBar />
             <Component {...pageProps} />
             <Footer />
+            <CrowdFundingProvide/>
         </>
     );
 }
